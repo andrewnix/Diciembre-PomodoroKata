169a0550ebbf2e4c3223918880a53510b053c06f
Andres Cardenas <akardenasjimenez@gmail.com>
2011-12-05 20:56:24 -0500

    Complement class Pomodoro and test class pomodoro and function sleep() on linux

--- Andrewnix/headerPomodoro.hpp
+++ Andrewnix/headerPomodoro.hpp
@@ -1,5 +1,14 @@
+/*
+ * headerPomodoro.hpp
+ *
+ * Autor: Andres F. Cardenas (Andrewnix)
+ *
+ * Este es el archivo cabecera que contiene la clase principal
+*/
+
 #include <iostream>
 #include <cstring>
+
 using namespace std;
 
 class Pomodoro
@@ -18,10 +27,14 @@ class Pomodoro
         int getInterrup();
         int getTime();
 
+        void setAddInterrup();
+        void setMinusTime();
+
     private:
         int time;
         int interrupciones;
 };
+
 
 /*
  * Inicializa el objeto Pomodoro con los valores por defecto
@@ -32,59 +45,106 @@ Pomodoro::Pomodoro()
     setInterrup(0);
 }
 
+
 /*
  * Finaliza el objeto Pomodoro
 */
 Pomodoro::~Pomodoro()
 {
-
+    cout << "\nDestruyendo pomodoro" << endl;
 }
 
+
+/*
+ * Arranca en pomodoro recibiendo un argumento por parte del usuario
+*/
 string Pomodoro::start(int minutos)
 {
     setTime(minutos);
-
+    cout << "Arrancando pomodoro" << endl;
     cout << "Tiempo establecido: " << getTime() << endl;
     cout << "Numero de interrupciones: " << getInterrup() << endl;
+
+    return "Pomodoro corriendo!!!";
 }
 
+
+/*
+ * Arranca pomodoro con el valor por defecto que es 25
+*/
 string Pomodoro::start()
 {
     cout << "Arrancando pomodoro" << endl;
     cout << "Tiempo establecido: " << getTime() << endl;
     cout << "Numero de interrupciones: " << getInterrup() << endl;
+
+    return "Pomodoro corriendo!!!";
 }
 
+
+/*
+ * Detiene el pomodoro, restaurando lo valores por defecto
+*/
 string Pomodoro::stop()
 {
-    cout << "parando pomodoro";
+    cout << "Deteniendo pomodoro";
     setTime(25);
     setInterrup(0);
+
+    return "Pomodoro detenido!!!";
 }
 
+
+/*
+ * Pausa el pomodoro aumentando el numero de interrupciones en 1
+*/
 string Pomodoro::pause()
 {
     getTime();
-    setInterrup(1);
+    setAddInterrup();
     getInterrup();
+
+    return "pomodoro pausado!!!";
 }
 
+
+/*
+ * Establece los minutos para el pomodoro
+*/
 void Pomodoro::setTime(int minutos)
-{
-    time = minutos;
-}
+{ time = minutos; }
 
+
+/*
+ * Disminuye en uno el atributo "time"
+*/
+void Pomodoro::setMinusTime()
+{ time--; }
+
+
+/*
+ * Establece el numero de interrupciones que lleva en el pomodoro
+*/
 void Pomodoro::setInterrup(int paramInter)
-{
-    interrupciones = paramInter;
-}
+{ interrupciones = paramInter; }
 
+
+/*
+ * Aumenta en uno el atributo "interrupciones"
+*/
+void Pomodoro::setAddInterrup()
+{ interrupciones++; }
+
+
+/*
+ * Devuelve el tiempo del pomodoro
+*/
 int Pomodoro::getTime()
-{
-    return time;
-}
+{ return time; }
 
+
+/*
+ * Devuelve el numero de interrupciones que se llevan
+*/
 int Pomodoro::getInterrup()
-{
-    return interrupciones;
-}
+{ return interrupciones; }
--- Andrewnix/main.cpp
+++ Andrewnix/main.cpp
@@ -1,12 +1,37 @@
+/*
+ * main.cpp
+ *
+ * Autor: Andres F. Cardenas (Andrewnix)
+ *
+ * Este es el archivo principal... donde comienza la ejecucion del programa
+*/
+
 #include <iostream>
 #include "headerPomodoro.hpp"
 
 using namespace std;
 
+/*
+ * --codigo de andamiaje--
+ * haciendo pruebas del funcionamiento de la clase
+ * Pomodoro y de la funcion sleep() en linux
+ * --codigo de andamiaje--
+*/
 int main()
 {
     Pomodoro pomodoro;
     pomodoro.start();
+    pomodoro.pause();
+    pomodoro.start();
+    pomodoro.pause();
+    pomodoro.start();
     pomodoro.stop();
+
+    for(int i = 1; i <= 3; i++)
+    {
+        cout << "\nhola" << i;
+        sleep(1);
+    }
+
     return 0;
 }
